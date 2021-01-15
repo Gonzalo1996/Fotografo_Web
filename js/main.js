b724b5f89c8af936fad9ed624f696365b30822fc
Gonzalo Alderete <69527966+Gonzalo1996@users.noreply.github.com>
2021-01-15 20:21:25 -0300

    Comienzo de página con su header e inicio. Completar su nav y terminar el responsive de inicio.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let slider = document.querySelector(".slider__contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido__slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .9s";
+    contador++;
+
+    if(contador == sliderIndividual.length){   
+        
+
+        contador = 1;
+        setTimeout(function(){
+            console.log("Ejecutando")
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+        },1500)
+    }
+}
